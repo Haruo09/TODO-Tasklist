d359ed3792fe095b65e03ea872593c67b738e225
Fábio Haruo de Freitas Nishioka <fabio.hn01@gmail.com>
2024-02-08 12:50:03 -0300

    alterações no App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { TodoForm } from './components/TodoForm';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Get Things Ready!</h1>
+      <TodoForm onInputChange={(e) => console.log(e.target.value)}/>
     </div>
   );
 }
